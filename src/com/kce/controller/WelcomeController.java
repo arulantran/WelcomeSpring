78cd82f6b8b9382799ce36dcf534b849cd377ffe
Arul Antran <Arul Antran@DESKTOP-DPJ9PPK>
2018-12-10 15:58:19 +0530

    version1

---
+++ src/com/kce/controller/WelcomeController.java
@@ -0,0 +1,21 @@
+package com.kce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WelcomeController {
+
+   @RequestMapping(value="/",method = RequestMethod.GET )
+   public String printWelcome(ModelMap model) {
+      model.addAttribute("message", "Hello Spring MVC Framework!");
+      return "Welcome";
+   }
+   @RequestMapping(value="/welcome",method = RequestMethod.GET )
+   public ModelAndView printHello() {   
+      return new ModelAndView("Welcome","message", "Spring MVC Framework!");
+   }
+}
